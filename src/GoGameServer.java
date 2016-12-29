355b0a23a76589f2f3b1bd0d07c886e005801f7d
hunqer <thehunqer@gmail.com>
2016-12-29 11:25:19 +0100

    First commit

---
+++ src/GoGameServer.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class GoGameServer {
+    /**
+     * Server for a network multi-player Go Game.
+     */
+
+
+}
