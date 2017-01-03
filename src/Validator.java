45482f122138ced38b921725c896f3fb2551d45a
altair259 <kamillo952@gmail.com>
2017-01-04 01:40:31 +0200

    Create Validator.java

---
+++ src/Validator.java
@@ -0,0 +1,12 @@
+
+public class Validator {
+	
+	 public boolean isLegal() {
+		return true;
+		
+	/* if(stone != Stone.NONE) return;
+     stone = whiteToMove ? Stone.WHITE : Stone.BLACK;
+     whiteToMove = !whiteToMove;
+     
+*/
+}}
