3162cbca33a61250e12389cb7fd1d66829702eea
altair259 <kamillo952@gmail.com>
2017-01-04 20:56:52 +0100

    Create Stone.java

---
+++ src/Stone.java
@@ -0,0 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.util.Random;
+
+public enum Stone { 
+
+    BLACK(Color.BLACK), WHITE(Color.WHITE), NONE(null);
+
+    final Color color;
+    private final static Random rand = new Random();
+
+    private Stone(Color c) {
+        color = c;
+    }
+
+    public void paint(Graphics g, int dimension) {
+        if(this == NONE) return;
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setColor(color);
+        int x = 5;
+        g2d.fillOval(rand.nextInt(x), rand.nextInt(x), dimension-x, dimension-x);
+    }
+}
