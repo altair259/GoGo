c2d2bc578835b67aee2fded6d35dc341a54f2cea
altair259 <kamillo952@gmail.com>
2017-01-04 20:52:43 +0100

    Update Bridge.java

--- src/Bridge.java
+++ src/Bridge.java
@@ -1,3 +1,8 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 import org.json.simple.JSONObject;
 
 public class Bridge implements Runnable {
@@ -7,9 +12,15 @@ public class Bridge implements Runnable {
 	private ObjectOutputStream out;
 	private ObjectInputStream in2;
 	private ObjectOutputStream out2;
+	private Validator validator = null;
+	private Boolean playerColor = null;
+	private JSONObject recivedData = null;
+	private JSONObject transmitData = null;
+	
 	public Bridge(Socket socket, Socket socket2) {
         this.socket = socket;
         this.socket2 = socket2;
+		validator = new Validator();
         try{
         	in = new ObjectInputStream(socket.getInputStream());
         	out = new ObjectOutputStream(socket.getOutputStream());
@@ -22,22 +33,60 @@ public class Bridge implements Runnable {
     }
 	@Override
 	public void run() {
-		System.out.println("Ruszył wątek panowie!");
+		System.out.println("Ruszył wątek !");
 		try {
-		        
-			JSONObject recivedData = (JSONObject) in.readObject();
-			int moveX = (int) recivedData.get("moveX");
-			int moveY = (int) recivedData.get("moveY");
-			Validator validator = new Validator();
-			boolean check = validator.isLegal(moveX,moveY);
-			JSONObject transmit1Data = new JSONObject();
-			transmit1Data.put("legal" , check);
+			int moveX, moveY;
+
+			transmitData = new JSONObject();
+			transmitData.put("color", (Boolean)true );
+			out.writeObject(transmitData);
+			transmitData.put("color", (Boolean)false );
+			out2.writeObject(transmitData);
 			
-			out.writeObject(transmit1Data);
+			while(true){
+
+				
+				transmitData = new JSONObject();
+				recivedData = new JSONObject();
+//				do{
+					recivedData = (JSONObject) in.readObject();
+//				}while(! validator.isLegal(
+//							(int)recivedData.get("moveX"),
+//							(int)recivedData.get("movey")
+//						));
+
+				System.out.println(recivedData);
+				transmitData.put("moveX", recivedData.get("moveX"));
+				transmitData.put("moveY", recivedData.get("moveY"));
+				transmitData.put("color", false);
+				transmitData.put("operation", "set");
+				out.writeObject(transmitData);
+				out.flush();
+				out2.writeObject(transmitData);
+				out2.flush();
+				
+				
+				transmitData = new JSONObject();
+				recivedData = new JSONObject();
+//				do{
+					recivedData = (JSONObject) in2.readObject();
+					System.out.println(recivedData);
+//				}while(! validator.isLegal(
+//							(int)recivedData.get("moveX"),
+//							(int)recivedData.get("movey")
+//						));
+
+				System.out.println(recivedData);
+				transmitData.put("moveX", recivedData.get("moveX"));
+				transmitData.put("moveY", recivedData.get("moveY"));
+				transmitData.put("color", true);
+				transmitData.put("operation", "set");
+				out.writeObject(transmitData);
+				out.flush();
+				out2.writeObject(transmitData);
+				out2.flush();
+			}
 		}catch(Exception e){}
-		// TODO Auto-generated method stub
-		
-		
 	}
 
-	}
+}
