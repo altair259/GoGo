93257e132f430e126df1c10e4efb0256c870f89e
altair259 <kamillo952@gmail.com>
2017-01-04 14:10:07 +0200

    Update Bridge.java

--- src/Bridge.java
+++ src/Bridge.java
@@ -1,34 +1,43 @@
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
-
 import org.json.simple.JSONObject;
 
 public class Bridge implements Runnable {
 	private Socket socket2;
 	private Socket socket;
+	private ObjectInputStream in;
+	private ObjectOutputStream out;
+	private ObjectInputStream in2;
+	private ObjectOutputStream out2;
 	public Bridge(Socket socket, Socket socket2) {
         this.socket = socket;
         this.socket2 = socket2;
+        try{
+        	in = new ObjectInputStream(socket.getInputStream());
+        	out = new ObjectOutputStream(socket.getOutputStream());
+        	in2 = new ObjectInputStream(socket2.getInputStream());
+        	out2 = new ObjectOutputStream(socket2.getOutputStream());
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+		
     }
 	@Override
 	public void run() {
-		
+		System.out.println("Ruszył wątek panowie!");
 		try {
-			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-			ObjectOutputStream out2 = new ObjectOutputStream(socket2.getOutputStream());
 		        
 			JSONObject recivedData = (JSONObject) in.readObject();
 			int moveX = (int) recivedData.get("moveX");
 			int moveY = (int) recivedData.get("moveY");
+			Validator validator = new Validator();
+			boolean check = validator.isLegal(moveX,moveY);
+			JSONObject transmit1Data = new JSONObject();
+			transmit1Data.put("legal" , check);
 			
-			
-			out2.writeObject();
+			out.writeObject(transmit1Data);
 		}catch(Exception e){}
 		// TODO Auto-generated method stub
 		
 		
 	}
 
-}
+	}
