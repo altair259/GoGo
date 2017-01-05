89c9d263a65d4aaca57a3ee447bf76c4de727576
altair259 <kamillo952@gmail.com>
2017-01-05 12:26:41 +0100

    Update Validator.java

--- src/Validator.java
+++ src/Validator.java
@@ -3,10 +3,13 @@ public class Validator {
 	
 	 public boolean isLegal() {
 		return true;
-		
-	/* if(stone != Stone.NONE) return;
-     stone = whiteToMove ? Stone.WHITE : Stone.BLACK;
-     whiteToMove = !whiteToMove;
-     
-*/
+		Stone[][] tab = new Stone[19][19];
+	
+	 public boolean isLegal(int moveX, int moveY, boolean color) {
+		 if(tab[moveX][moveY] == null){
+			 return true;
+		 }else{
+			 return false;
+		 }
+
 }}
