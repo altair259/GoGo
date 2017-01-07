1c2b7c4103b7b850b9c4e3360e488553477224c9
altair259 <kamillo952@gmail.com>
2017-01-07 18:22:07 +0100

    Update Bridge.java

--- src/Bridge.java
+++ src/Bridge.java
@@ -1,7 +1,9 @@
+import java.awt.Point;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import org.json.simple.JSONObject;
 
@@ -16,6 +18,7 @@ public class Bridge implements Runnable {
 	private Boolean playerColor = null;
 	private JSONObject recivedData = null;
 	private JSONObject transmitData = null;
+	private ArrayList<Point> forRemove;
 	
 	public Bridge(Socket socket, Socket socket2) {
         this.socket = socket;
@@ -33,14 +36,14 @@ public class Bridge implements Runnable {
     }
 	@Override
 	public void run() {
-		System.out.println("Ruszył wątek !");
+		System.out.println("Ruszył wątek panowie!");
 		try {
 			int moveX, moveY;
 
 			transmitData = new JSONObject();
-			transmitData.put("color", (Boolean)true );
+			transmitData.put("color", Stone.WHITE );
 			out.writeObject(transmitData);
-			transmitData.put("color", (Boolean)false );
+			transmitData.put("color", Stone.BLACK );
 			out2.writeObject(transmitData);
 			
 			while(true){
@@ -48,43 +51,55 @@ public class Bridge implements Runnable {
 				
 				transmitData = new JSONObject();
 				recivedData = new JSONObject();
-//				do{
+				do{
 					recivedData = (JSONObject) in.readObject();
-//				}while(! validator.isLegal(
-//							(int)recivedData.get("moveX"),
-//							(int)recivedData.get("movey")
-//						));
+				}while(! validator.isLegal(
+							(int)recivedData.get("moveX"),
+							(int)recivedData.get("moveY"),
+							Stone.BLACK
+						));
 
 				System.out.println(recivedData);
+				forRemove = validator.getPlacesForRemove();
 				transmitData.put("moveX", recivedData.get("moveX"));
 				transmitData.put("moveY", recivedData.get("moveY"));
-				transmitData.put("color", false);
+				transmitData.put("color", Stone.BLACK);
 				transmitData.put("operation", "set");
+				transmitData.put("listForRemove", forRemove);
 				out.writeObject(transmitData);
 				out.flush();
 				out2.writeObject(transmitData);
 				out2.flush();
+				
+				
+				System.out.println(forRemove);
+
+				
 				
 				
 				transmitData = new JSONObject();
 				recivedData = new JSONObject();
-//				do{
+				do{
 					recivedData = (JSONObject) in2.readObject();
 					System.out.println(recivedData);
-//				}while(! validator.isLegal(
-//							(int)recivedData.get("moveX"),
-//							(int)recivedData.get("movey")
-//						));
+				}while(! validator.isLegal(
+							(int)recivedData.get("moveX"),
+							(int)recivedData.get("moveY"),
+							Stone.WHITE
+						));
 
 				System.out.println(recivedData);
+				forRemove = validator.getPlacesForRemove();
 				transmitData.put("moveX", recivedData.get("moveX"));
 				transmitData.put("moveY", recivedData.get("moveY"));
-				transmitData.put("color", true);
+				transmitData.put("color", Stone.WHITE);
+				transmitData.put("listForRemove", forRemove);
 				transmitData.put("operation", "set");
 				out.writeObject(transmitData);
 				out.flush();
 				out2.writeObject(transmitData);
 				out2.flush();
+				
 			}
 		}catch(Exception e){}
 	}
