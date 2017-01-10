0c84a380b94ac6a060cd81772f4924c166261067
altair259 <kamillo952@gmail.com>
2017-01-10 01:10:20 +0100

    Update Board.java

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.io.ObjectInputStream;
 import java.net.Socket;
-import java.util.Random;
 import javax.swing.*;
 
 public class Board {
@@ -13,12 +11,22 @@ public class Board {
     	panel = new GoPanel(19, socket);
     	this.socket = socket;
         JFrame frame = new JFrame();
-        frame.setLayout(new BorderLayout());
-        frame.add(panel, BorderLayout.CENTER);
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+        pane.add(panel, BorderLayout.CENTER);
+        JButton button = new JButton("Pasuj xD");
+        button.setMinimumSize(new Dimension(100, 50));
+        button.setSize(new Dimension(100, 50));
+        pane.add(button, pane.CENTER_ALIGNMENT);
+        pane.setMinimumSize(new Dimension(1200, 1330));
+        frame.setMinimumSize(new Dimension(1200, 1330));
+        frame.setSize(new Dimension(1200, 1330));
+        
+        
     	System.out.println("tutaj doszedłem w Board!");
-        frame.setSize(1200, 1250);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
+        frame.pack();
         frame.setVisible(true);
         panel.startGame();
         System.out.print("tutaj doszedłem w Board11!");
