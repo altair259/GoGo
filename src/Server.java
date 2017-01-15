adc56056c24d5caadd880abc819a9d09986be3ea
altair259 <kamillo952@gmail.com>
2017-01-15 21:13:59 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,29 @@
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+class ConnectionHandler {
+	
+	public Bridge getConnection(ServerSocket serverSocket) throws IOException {
+		System.out.println("Server started");
+		Socket socket = serverSocket.accept();
+		System.out.println("First accepted");
+		Socket socket2 = serverSocket.accept();
+		System.out.println("Second accepted");
+		Bridge bridge = new Bridge(socket, socket2);
+		System.out.println("Created Runnable Object");
+		return bridge;
+	}
+	
+	public void startConnection(Bridge bridge) throws InterruptedException {
+		(new Thread(bridge)).start();
+		while(true){
+			Thread.sleep(40);
+//			serverSocket.accept();
+		}
+	}
+}
 
 public class Server {
 
@@ -9,24 +32,14 @@ public class Server {
 	static DataOutputStream out;
 	static Socket socket2;
 	
+	public void init(ConnectionHandler cf, ServerSocket ss) throws Exception{
+		cf.startConnection(cf.getConnection(ss));
+	}
 
-	public static void main(String[] args)throws Exception{
-		 Socket socket = null;
-		 Socket socket2 = null;
+	public static void main(String[] args) throws Exception{
+		Server server = new Server();
 		System.out.println("Starting server..");
-		serverSocket = new ServerSocket(7777);
-		System.out.println("Server started");
-		socket = serverSocket.accept();
-		System.out.println("First accepted");
-		socket2 = serverSocket.accept();
-		System.out.println("Second accepted");
-		Bridge bridge = new Bridge(socket, socket2);
-		System.out.println("Created Runnable Object");
-		(new Thread(bridge)).start();
-		while(true){
-			Thread.sleep(40);
-//			serverSocket.accept();
-		}
+		server.init(new ConnectionHandler(), new ServerSocket(7777));
 	}
 }
 
