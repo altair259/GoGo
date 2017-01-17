3f5b58e0a5b288729efe6d1f6a2702a93d90bb71
altair259 <kamillo952@gmail.com>
2017-01-17 02:25:46 +0100

    Update Validator.java

--- src/Validator.java
+++ src/Validator.java
@@ -1,7 +1,10 @@
 
+package program;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashSet;
+
+import com.sun.prism.paint.Color;
 
 public class Validator {
 	
@@ -14,26 +17,37 @@ public class Validator {
 	
 	 public boolean isLegal(int x, int y, Stone color) {
 		 System.out.println(color);
-		 if(tab[x][y] == null && isSuicide(x,y,color) /*&& !checkKo(x,y,color)*/){
+		 if(tab[x][y] == null && !isSuicide(x,y,color) && !checkKo(x,y,color)){
+			 System.out.println(x + ", " + y + " is OK!");
 			 tab[x][y] = color;
 			 updateStones(x,y,color);
+			 this.backupBoard();
 			 return true;
 		 }else{
 			 return false;
 		 }
 	}
+	
+	/**
+	 * Update stones.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param color the color
+	 */
 	void updateStones(int x, int y, Stone color){
 		Stone invertedColor = (color == Stone.WHITE ? Stone.BLACK : Stone.WHITE);
-		if(DFS(x,y+1, invertedColor) == false){
+		
+		if(!DFS(x,y+1, invertedColor)){
 			removeGroupOfStones(x,y+1,invertedColor);
 		}
-		if(DFS(x,y-1, invertedColor) == false){
+		if(!DFS(x,y-1, invertedColor)){
 			removeGroupOfStones(x,y-1,invertedColor);
 		}
-		if(DFS(x+1,y, invertedColor) == false){
+		if(!DFS(x+1,y, invertedColor)){
 			removeGroupOfStones(x+1,y,invertedColor);
 		}
-		if(DFS(x-1,y, invertedColor) == false){
+		if(!DFS(x-1,y, invertedColor)){
 			removeGroupOfStones(x-1,y,invertedColor);
 		}
 	}
@@ -45,26 +59,34 @@ public class Validator {
 	}
 	
 	private void removeGroupOfStones(int x, int y ,Stone color){
-		System.out.print(x + " " + y);
+		
 		if( x == -1 || x == 19 || y == -1 || y == 19 || tab[x][y] == null || tab[x][y] != color){
 			return;
 		}
+		System.out.println("Remove " + x + " " + y);
 	
 		
 		forRemove.add(new Point(x,y));
-		tab[x][y]= null;
+		tab[x][y] = null;
 		
 		removeGroupOfStones(x,y+1,color);
 		removeGroupOfStones(x,y-1,color);
 		removeGroupOfStones(x+1,y,color);
 		removeGroupOfStones(x-1,y,color);
+		
 	}
 	
 	
 	public boolean isSuicide( int x, int y, Stone color){
 		tab[x][y] = color;
 		visited = new HashSet<Point>();
-		boolean result = DFS(x,y,color);
+		Stone invertedColor = (color == Stone.WHITE ? Stone.BLACK : Stone.WHITE);
+		boolean result = true;
+		result &= !DFS(x,y,color);
+		result &= DFS(x+1, y, invertedColor);
+		result &= DFS(x-1, y, invertedColor);
+		result &= DFS(x, y+1, invertedColor);
+		result &= DFS(x, y-1, invertedColor);
 		tab[x][y] = null;
 		return result;
 	}
@@ -79,38 +101,66 @@ public class Validator {
 		
 		}
 	}
-	public boolean checkKo(int k, int l, Stone color){
-		boolean isKo;
+	public boolean checkKo(int x, int y, Stone color){
+		boolean isKo = false;
 		Stone [][] backupTab3 = new Stone[19][19];
-		for(int i=0; i<tab.length; i++){ 
-            for(int j=0; j<tab[i].length; j++){ 
-                backupTab3[i][j] = tab[i][j];
+		for(int i = 0; i < tab.length; i++){ 
+            for(int j = 0; j < tab[i].length; j++){
+            	backupTab3[i][j] = tab[i][j];
             }
 		}
-		tab[k][l] = color;
-		updateStones(k,l,color);
+
+		System.out.println("AAAAAAAAAAAAA " + x + ", " + y + " checking KO");
+		tab[x][y] = color;
+		forRemove = new ArrayList<Point>();
+		updateStones(x, y, color);
+		forRemove = new ArrayList<Point>();
 		isKo = true;
 		for(int i=0; i<tab.length; i++){ 
             for(int j=0; j<tab[i].length; j++){ 
-                if(tab[k][l] != backupTab2[i][j]){
+                if(tab[i][j] != backupTab2[i][j]){
+                	System.out.println("tablice różnią się w " + i + " " + j + " :) ");
                 	 isKo = false;
                 } 	
             }
 	
 		}
-		for(int i=0; i<backupTab3.length; i++){ 
-            for(int j=0; j<backupTab3[i].length; j++){ 
-                tab[i][j] = backupTab3[i][j];
+		for(int i = 0; i < tab.length; i++){ 
+            for(int j = 0; j < tab[i].length; j++){
+            	tab[i][j] = backupTab3[i][j];
             }
 		}
 		return isKo;
 	}
 	
 	
+	/**
+	 * Check if Point(x,y) has path to null.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param color the color
+	 * @return True if Point(x,y) has path to null.
+	 */
 	
-	public boolean DFS(int x, int y, Stone color){
+	private boolean DFS(int x, int y, Stone color){
+		visited = new HashSet<Point>();
+		boolean result = DFSRecursive(x, y, color);
+		System.out.println("DFS from " + x + ", " + y + " result: " + result);
+		return result;
+	}
+	
+	/**
+	 * DFS recursive.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param color the color
+	 * @return true, if successful
+	 */
+	private boolean DFSRecursive(int x, int y, Stone color){
 		if(visited.contains(new Point(x,y)) || x == -1 || x == 19 || y == -1 || y == 19 || (tab[x][y] != null && tab[x][y] != color)){
-			System.out.println(x + " " + y + " " + color + " return false");
+			System.out.println(x + " " + y + " " + (color==null?"null":color) + " return false");
 			return false;
 		}
 		
@@ -119,10 +169,10 @@ public class Validator {
 			return true;
 		}
 		return (
-				DFS(x,y+1, color) ||
-				DFS(x,y-1, color) ||
-				DFS(x+1,y, color) ||
-				DFS(x-1,y,color)
+				DFSRecursive(x,y+1, color) ||
+				DFSRecursive(x,y-1, color) ||
+				DFSRecursive(x+1,y, color) ||
+				DFSRecursive(x-1,y,color)
 		);
 	}
 	
