d432da8715b7b8a2e957ed26a0349e316f9ceb0f
altair259 <kamillo952@gmail.com>
2017-01-21 21:36:28 +0100

    Update Stone.java

--- src/Stone.java
+++ src/Stone.java
@@ -1,19 +1,38 @@
+package program;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.util.Random;
 
+
+/**
+ * The Enum Stone.
+ */
 public enum Stone { 
 
-    BLACK(Color.BLACK), WHITE(Color.WHITE), NONE(null);
+    BLACK(Color.BLACK),
+ WHITE(Color.WHITE), 
+ NONE(null);
 
+    /** The color. */
     final Color color;
 
+    /**
+     * Instantiates a new stone.
+     *
+     * @param c the c
+     */
     private Stone(Color c) {
         color = c;
     }
-
+    
+    /**
+     * Paint.
+     *
+     * @param g the g
+     * @param dimension the dimension
+     */
     public void paint(Graphics g, int dimension) {
         if(this == NONE) return;
         Graphics2D g2d = (Graphics2D)g;
