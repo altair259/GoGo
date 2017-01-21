1e7874566f323853128785f6cb604be3b21a02af
altair259 <kamillo952@gmail.com>
2017-01-21 21:32:43 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,29 +1,9 @@
+package program;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-class ConnectionHandler {
-	
-	public Bridge getConnection(ServerSocket serverSocket) throws IOException {
-		System.out.println("Server started");
-		Socket socket = serverSocket.accept();
-		System.out.println("First accepted");
-		Socket socket2 = serverSocket.accept();
-		System.out.println("Second accepted");
-		Bridge bridge = new Bridge(socket, socket2);
-		System.out.println("Created Runnable Object");
-		return bridge;
-	}
-	
-	public void startConnection(Bridge bridge) throws InterruptedException {
-		(new Thread(bridge)).start();
-		while(true){
-			Thread.sleep(40);
-//			serverSocket.accept();
-		}
-	}
-}
 
 public class Server {
 
@@ -36,10 +16,17 @@ public class Server {
 		cf.startConnection(cf.getConnection(ss));
 	}
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws Exception the exception
+	 */
 	public static void main(String[] args) throws Exception{
 		Server server = new Server();
 		System.out.println("Starting server..");
 		server.init(new ConnectionHandler(), new ServerSocket(7777));
 	}
 }
+
 
